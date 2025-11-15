8269ef35a138586a6b46a5ce42f977354eb4bf97
Cubits11 <90584946+Cubits11@users.noreply.github.com>
2025-11-15 16:55:29 -0500

    Expose API functions in cc_surface module

--- src/gce/core/cc_surface/__init__.py
+++ src/gce/core/cc_surface/__init__.py
@@ -1,1 +1,9 @@
+from .api import compute_verdict, analyze_composition, fh_bounds, backend_info, format_verdict
 
+__all__ = [
+    "compute_verdict",
+    "analyze_composition",
+    "fh_bounds",
+    "backend_info",
+    "format_verdict",
+]
